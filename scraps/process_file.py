bab3c2b5dea9194e340eae185012d9530e17f911
Faustin Carter <faustin.carter@gmail.com>
2018-09-20 15:02:04 -0500

    Add ability to return just metadata from process file

--- scraps/process_file.py
+++ scraps/process_file.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
+
 try: import simplejson as json
 except ImportError: import json
 
-def process_file(fileName, mask = None, **loadtxt_kwargs):
+def process_file(fileName, mask = None, meta_only=False, **loadtxt_kwargs):
     """Load Keysight PNA file data into dict.
 
     Parameters
@@ -20,12 +22,16 @@ def process_file(fileName, mask = None, **loadtxt_kwargs):
     mask : slice (optional)
         You can pass a slice to cut out some region of data. Example 1: mask = slice(10,-10) will cut out the first 10 and last 10 datapoints. Example 2: maks = slice(None,None,10) will resample your data and take every 10th point. Example 3: mask = slice(None, -50) will cut the last 50 points off your data.
 
+    meta_only : bool
+        Return just the metadata from the filename (True) or all the data (False).
+        Default is False.
+    
     loadtxt_kwargs : dict (optional)
         This is a pass-through to numpy.loadtxt
 
     Returns
     -------
-    dataDict : dict or ``None``
+    dict or ``None``
         Dictionary contains the following keys: 'name', 'temp', 'pwr', 'freq',
         'I', 'Q'. If fileName does not exist, then returns ``None``.
 
@@ -56,14 +62,24 @@ def process_file(fileName, mask = None, **loadtxt_kwargs):
 
         resName = fileName[resNameLoc:resNameLoc+5]
 
-        #Grab frequency, I, and Q
-        fileData = np.loadtxt(fileName, **loadtxt_kwargs)
-        freqData = fileData[:,0][mask]
-        IData = fileData[:,1][mask]
-        QData = fileData[:,2][mask]
+        metaDict = {'name':resName,'temp':temp,'pwr':pwr}
 
-        dataDict = {'name':resName,'temp':temp,'pwr':pwr,'freq':freqData,'I':IData,'Q':QData}
-        return dataDict
+        if meta_only:
+            dataDict = {}
+        else:
+            #Grab frequency, I, and Q
+            fileData = np.loadtxt(fileName, **loadtxt_kwargs)
+            freqData = fileData[:,0][mask]
+            IData = fileData[:,1][mask]
+            QData = fileData[:,2][mask]
+
+            dataDict = {'freq':freqData,'I':IData,'Q':QData}
+
+        retVal = {}
+        retVal.update(metaDict)
+        retVal.update(dataDict)
+        
+        return retVal
     else:
         
         assert False, "Bad file? " + fileName
